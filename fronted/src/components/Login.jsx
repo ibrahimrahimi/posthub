239995ec2d8950fd2f2366c7fb3d79e3b25acbd8
Ibrahim Rahimi <ebrahimrahimilaziri@gmail.com>
2023-01-20 15:47:25 +0300

    Update project dependencies and add google user login feature.

---
+++ fronted/src/components/Login.jsx
@@ -0,0 +1,68 @@
+import React from 'react';
+import {GoogleLogin, useGoogleLogin} from '@react-oauth/google';
+import { useNavigate } from 'react-router-dom';
+import { FcGoogle, FcNavigate } from 'react-icons/fc';
+import shareVideo from '../assets/share.mp4';
+import logo from '../assets/logowhite.png';
+import axios from 'axios';
+
+
+const Login = () => {
+	const login = useGoogleLogin({
+		onSuccess: async respone => {
+			try {
+					const data = await axios.get('https://www.googleapis.com/oauth2/v3/userinfo', {
+					headers: {
+						'Authorization': `Bearer ${respone.access_token}`
+					}
+				})
+				console.log(data.data);
+				localStorage.setItem('user', JSON.stringify(data.data));
+				const {name, sub, picture} = data.data;
+				console.log(name, sub, picture);
+				const userDetails = {
+					_id: sub,
+					_type: 'user',
+					userName: name,
+					image: picture,
+				}
+			} catch(error) {
+				console.log(error);
+			}
+			  
+		}
+	});
+
+	return (
+		<div className="flex justify-start items-center flex-col h-screen">
+			<div className="relative w-full h-full">
+				<video
+					src={shareVideo}
+					type="video/mp4"
+					loop
+					controls={false}
+					muted
+					autoPlay
+					className="w-full h-full object-cover"
+				/>
+				<div className='absolute flex flex-col justify-center items-center top-0 right-0 left-0 bottom-0 bg-blackOverlay'>
+					<div className='p-5'>
+						<img src={logo} width="130px" alt="logo" />
+					</div>
+					<div className='shadow-2xl'>
+						<button type='button'
+								className='bg-mainColor flex justify-center items-center p-3 rounded cursor-pointer outline-none'
+								onClick={() => login()}
+								// disabled={renderProps.disabled}
+							>
+								<FcGoogle className='mr-2' /> Sign in with Google
+						</button>
+						
+					</div>
+				</div>
+			</div>
+		</div>
+	)
+}
+
+export default Login
--- fronted/src/index.js
+++ fronted/src/index.js
@@ -1,12 +1,17 @@
 import React from "react";
-import ReactDOM  from "react-dom";
+import ReactDOM  from "react-dom/client";
 import { BrowserRouter as Router } from "react-router-dom";
+import { GoogleOAuthProvider } from '@react-oauth/google';
 
 import App from './App';
+import './index.css';
 
-ReactDOM.render(
+const root = ReactDOM.createRoot(document.getElementById('root'));
+
+root.render(
     <Router>
-        <App />, 
-    </Router>,
-    document.getElementById('root')
+        <GoogleOAuthProvider clientId={process.env.REACT_APP_GOOGLE_API_TOKEN} >
+            <App /> 
+        </GoogleOAuthProvider>
+    </Router>
 );
