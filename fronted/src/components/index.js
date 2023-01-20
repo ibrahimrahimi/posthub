cbccf4ae33a8d9807371351b25d2e1969a30b3c2
Ibrahim Rahimi <ebrahimrahimilaziri@gmail.com>
2023-01-21 00:24:08 +0300

    Add sidebar component and toggleSidebar feature.

--- fronted/src/components/index.js
+++ fronted/src/components/index.js
@@ -1,3 +1,3 @@
-export { default as SideBar } from './SideBar';
+export { default as Sidebar } from './Sidebar';
 export { default as UserProfile } from './UserProfile';
 export { default as Login } from './Login';
