6f7db7112d42ae0777728b9f2b0896d200703548
Ibrahim Rahimi <ebrahimrahimilaziri@gmail.com>
2023-01-21 18:22:11 +0300

    Add search query for seaching feeds and complete Feed component.

--- fronted/src/components/Feed.jsx
+++ fronted/src/components/Feed.jsx
@@ -1,16 +1,42 @@
 import React, { useState, useEffect } from 'react';
-import { useParamss } from 'react-router-dom';
+import { useParams } from 'react-router-dom';
 
 import { client } from '../client';
+import { feedQuery, searchQuery } from '../utils/data';
 import MasonryLayout from './MasonryLayout';
 import Spinner from './Spinner';
 
 function Feed() {
     const [loading, setLoading] = useState(false);
+    const [pins, setPins] = useState();
+    const { categoryId } = useParams();
+
+    useEffect(() => {
+        setLoading(true);
+        
+        if(categoryId) {
+            const query = searchQuery(categoryId);
+            client.fetch(query).then((data) => {
+                setPins(data);
+                setLoading(false);
+            });
+        } else {
+            setLoading(true);
+
+            client.fetch(feedQuery).then((data) => {
+                setPins(data);
+                setLoading(false);
+            });
+        }
+        
+        }, [categoryId]);
     
-    if(loading) return <Spinner message="We are adding new ideas to your feed!"/>    
+    const ideaName = categoryId || 'new';
+    if(loading) return <Spinner message={`We are adding ${ideaName} ideas to your feed!`}/>    
     return (
-        <div>Feed</div>
+       <div>
+            {pins && (<MasonryLayout pins={pins} />)}
+       </div>
     );
 };
 
